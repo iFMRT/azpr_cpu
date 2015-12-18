ca418fb31fdf06898bfaaa8accdf4d7eb4c7c7f2
GrinV <colin4124@gmail.com>
2015-12-18 15:30:39 +0800

    translate cpu.h to Chinese

--- rtl/cpu/include/cpu.h
+++ rtl/cpu/include/cpu.h
@@ -14,100 +14,100 @@
 //------------------------------------------------------------------------------
 // Operation
 //------------------------------------------------------------------------------
-	/********** レジスタ **********/
-	`define REG_NUM				 32	  // レジスタ数
-	`define REG_ADDR_W			 5	  // レジスタアドレス幅
-	`define RegAddrBus			 4:0  // レジスタアドレスバス
-	/********** 割り込み要求信号 **********/
-	`define CPU_IRQ_CH			 8	  // IRQ幅
-	/********** ALUオペコード **********/
-	// バス
-	`define ALU_OP_W			 4	  // ALUオペコード幅
-	`define AluOpBus			 3:0  // ALUオペコードバス
-	// オペコード
+	/********** 寄存器 **********/
+  `define REG_NUM				 32	  //  寄存器数
+	`define REG_ADDR_W			 5	  // 寄存器地址宽度
+	`define RegAddrBus			 4:0  // 寄存器地址总线
+	/********** 中断请求信号 **********/
+	`define CPU_IRQ_CH			 8	  // IRQ宽
+	/********** ALU 操作码 **********/
+	// 总线
+	`define ALU_OP_W			 4	  // ALU操作码宽
+	`define AluOpBus			 3:0  // ALU操作码总线
+	// 操作码
 	`define ALU_OP_NOP			 4'h0 // No Operation
 	`define ALU_OP_AND			 4'h1 // AND
 	`define ALU_OP_OR			 4'h2 // OR
 	`define ALU_OP_XOR			 4'h3 // XOR
-	`define ALU_OP_ADDS			 4'h4 // 符号付き加算
-	`define ALU_OP_ADDU			 4'h5 // 符号なし加算
-	`define ALU_OP_SUBS			 4'h6 // 符号付き減算
-	`define ALU_OP_SUBU			 4'h7 // 符号なし減算
-	`define ALU_OP_SHRL			 4'h8 // 論理右シフト
-	`define ALU_OP_SHLL			 4'h9 // 論理左シフト
-	/********** メモリオペコード **********/
-	// バス
-	`define MEM_OP_W			 2	  // メモリオペコード幅
-	`define MemOpBus			 1:0  // メモリオペコードバス
-	// オペコード
+	`define ALU_OP_ADDS			 4'h4 // 有符号加法
+	`define ALU_OP_ADDU			 4'h5 // 无符号加法
+	`define ALU_OP_SUBS			 4'h6 // 有符号减法
+	`define ALU_OP_SUBU			 4'h7 // 无符号减法
+	`define ALU_OP_SHRL			 4'h8 // 逻辑右移
+	`define ALU_OP_SHLL			 4'h9 // 逻辑左移
+	/********** 内存操作码 **********/
+	// 总线
+	`define MEM_OP_W			 2	  // 内存操作码宽
+	`define MemOpBus			 1:0  // 内存操作码总线
+	// 操作码
 	`define MEM_OP_NOP			 2'h0 // No Operation
-	`define MEM_OP_LDW			 2'h1 // ワード読み出し
-	`define MEM_OP_STW			 2'h2 // ワード書き込み
-	/********** 制御オペコード **********/
-	// バス
-	`define CTRL_OP_W			 2	  // 制御オペコード幅
-	`define CtrlOpBus			 1:0  // 制御オペコードバス
-	// オペコード
+	`define MEM_OP_LDW			 2'h1 // 字读取
+	`define MEM_OP_STW			 2'h2 // 字写入
+	/********** 控制操作码 **********/
+	// 总线
+	`define CTRL_OP_W			 2	  // 控制操作码宽
+	`define CtrlOpBus			 1:0  // 控制操作码总线
+	// 操作码
 	`define CTRL_OP_NOP			 2'h0 // No Operation
-	`define CTRL_OP_WRCR		 2'h1 // 制御レジスタへの書き込み
-	`define CTRL_OP_EXRT		 2'h2 // 例外からの復帰
+	`define CTRL_OP_WRCR		 2'h1 // 写入控制寄存器
+	`define CTRL_OP_EXRT		 2'h2 // 从异常恢复
 
-	/********** 実行モード **********/
-	// バス
-	`define CPU_EXE_MODE_W		 1	  // 実行モード幅
-	`define CpuExeModeBus		 0:0  // 実行モードバス
-	// 実行モード
-	`define CPU_KERNEL_MODE		 1'b0 // カーネルモード
-	`define CPU_USER_MODE		 1'b1 // ユーザモード
+	/********** 执行模式 **********/
+	// 总线
+	`define CPU_EXE_MODE_W		 1	  // 执行模式宽
+	`define CpuExeModeBus		 0:0  // 执行模式总线
+	// 执行模式
+	`define CPU_KERNEL_MODE		 1'b0 // 内核模式
+	`define CPU_USER_MODE		 1'b1 // 用户模式
 
 //------------------------------------------------------------------------------
-// 制御レジスタ
+// 控制寄存器
 //------------------------------------------------------------------------------
 	/********** アドレスマップ **********/
-	`define CREG_ADDR_STATUS	 5'h0  // ステータス
-	`define CREG_ADDR_PRE_STATUS 5'h1  // 前のステータス
-	`define CREG_ADDR_PC		 5'h2  // プログラムカウンタ
-	`define CREG_ADDR_EPC		 5'h3  // 例外プログラムカウンタ
-	`define CREG_ADDR_EXP_VECTOR 5'h4  // 例外ベクタ
-	`define CREG_ADDR_CAUSE		 5'h5  // 例外原因レジスタ
-	`define CREG_ADDR_INT_MASK	 5'h6  // 割り込みマスク
-	`define CREG_ADDR_IRQ		 5'h7  // 割り込み要求
+	`define CREG_ADDR_STATUS	 5'h0  // 状态
+	`define CREG_ADDR_PRE_STATUS 5'h1  // 前一个状态
+	`define CREG_ADDR_PC		 5'h2  // 程序计数器
+	`define CREG_ADDR_EPC		 5'h3  // 异常程序计数器
+	`define CREG_ADDR_EXP_VECTOR 5'h4  // 异常向量
+	`define CREG_ADDR_CAUSE		 5'h5  // 异常原因寄存器
+	`define CREG_ADDR_INT_MASK	 5'h6  // 中断掩字
+	`define CREG_ADDR_IRQ		 5'h7  // 中断请求
 	// 読み出し専用領域
-	`define CREG_ADDR_ROM_SIZE	 5'h1d // ROMサイズ
-	`define CREG_ADDR_SPM_SIZE	 5'h1e // SPMサイズ
-	`define CREG_ADDR_CPU_INFO	 5'h1f // CPU情報
+	`define CREG_ADDR_ROM_SIZE	 5'h1d // ROM容量
+	`define CREG_ADDR_SPM_SIZE	 5'h1e // SPM容量
+	`define CREG_ADDR_CPU_INFO	 5'h1f // CPU参数
 	/********** ビットマップ **********/
-	`define CregExeModeLoc		 0	   // 実行モードの位置
-	`define CregIntEnableLoc	 1	   // 割り込み有効の位置
-	`define CregExpCodeLoc		 2:0   // 例外コードの位置
-	`define CregDlyFlagLoc		 3	   // ディレイスロットフラグの位置
+	`define CregExeModeLoc		 0	   // 执行模式的位置
+	`define CregIntEnableLoc	 1	   // 中断有效的位置
+	`define CregExpCodeLoc		 2:0   // 异常代码的位置
+	`define CregDlyFlagLoc		 3	   // 延迟间隙标志位的位置
 
 //------------------------------------------------------------------------------
-// バスインタフェース
+// 总线接口
 //------------------------------------------------------------------------------
-	/********** バスインタフェースの状態 **********/
-	// バス
-	`define BusIfStateBus		 1:0   // 状態バス
-	// 状態
-	`define BUS_IF_STATE_IDLE	 2'h0  // アイドル
-	`define BUS_IF_STATE_REQ	 2'h1  // バスリクエスト
-	`define BUS_IF_STATE_ACCESS	 2'h2  // バスアクセス
-	`define BUS_IF_STATE_STALL	 2'h3  // ストール
+	/********** 总线接口的状态 **********/
+	// 总线
+	`define BusIfStateBus		 1:0   // 状态总线
+	// 状态
+	`define BUS_IF_STATE_IDLE	 2'h0  // 空闲
+	`define BUS_IF_STATE_REQ	 2'h1  // 请求总线
+	`define BUS_IF_STATE_ACCESS	 2'h2  // 访问总线
+	`define BUS_IF_STATE_STALL	 2'h3  // 停滞
 
 //------------------------------------------------------------------------------
 // MISC
 //------------------------------------------------------------------------------
-	/********** ベクタ **********/
-	`define RESET_VECTOR		 30'h0 // リセットベクタ
-	/********** シフト量 **********/
-	`define ShAmountBus			 4:0   // シフト量バス
-	`define ShAmountLoc			 4:0   // シフト量の位置
+	/********** 向量 **********/
+	`define RESET_VECTOR		 30'h0 // 复位向量
+	/********** 移位量 **********/
+	`define ShAmountBus			 4:0   // 移位量总线
+	`define ShAmountLoc			 4:0   // 移位量的位置
 
-	/********** CPU情報 *********/
-	`define RELEASE_YEAR		 8'd41 // 作成年 (YYYY - 1970)
-	`define RELEASE_MONTH		 8'd7  // 作成月
-	`define RELEASE_VERSION		 8'd1  // バージョン
-	`define RELEASE_REVISION	 8'd0  // リビジョン
+	/********** CPU参数 *********/
+	`define RELEASE_YEAR		 8'd41 // 制作年度 (YYYY - 1970)
+	`define RELEASE_MONTH		 8'd7  // 制作月份
+	`define RELEASE_VERSION		 8'd1  // 版本号
+	`define RELEASE_REVISION	 8'd0  // 修订号
 
 
 `endif
